11279c1e1f64569e135a27c25f186203ddba49e0
Martin Stokroos <44379672+MartinStokroos@users.noreply.github.com>
2018-12-19 15:20:35 +0100

    Update adaptive_Hopf.m
    changes in comments

--- adaptive_Hopf.m
+++ adaptive_Hopf.m
@@ -15,15 +15,15 @@ clear all;
 close all;
 clc;
 
-Fs=1000; %Sampling frequency
-Tfinal=10.0; %simulation time
-T=1/Fs; %Sampling Time = 1/Fs
-t=linspace(0,Tfinal,Tfinal/T); %simulation time vector
+Fs=1000; % sampling frequency
+Tfinal=10.0; % simulation time
+T=1/Fs; % sampling Time = 1/Fs
+t=linspace(0,Tfinal,Tfinal/T); % simulation time vector
 
 plotvar1 = linspace(0,0,Tfinal/T);
 plotvar2 = linspace(0,0,Tfinal/T);
 plotvar3 = linspace(0,0,Tfinal/T);
-x = 1;  %starting point of the oscillator. x=1 and y=0, starts as cosine. 
+x = 1;  % starting point of the oscillator. x=1 and y=0, starts as cosine. 
 x_new = 0;
 x_d = 0;
 y = 0;
@@ -33,17 +33,17 @@ ohm = 10*2*pi; %10Hz
 ohm_new = 0;
 ohm_d = 0;
 
-Epsilon = 15; %coupling strength (>0).
-              %the oscillator locs faster for larger values, but it will also increase the ripple in the frequency. 
+Epsilon = 15; % coupling strength (>0).
+              % the oscillator locks faster for larger values, but it will also increase the ripple in the frequency. 
 
-gamma = 100.0; %follows the amplitude of the stimulus better for larger values, but it may cause instability.  
+gamma = 100.0; % follows the amplitude of the stimulus better for larger values, but it may cause instability.  
 
-mu = 1.0; %seems to scale the amplitude.
+mu = 1.0; % seems to scale the amplitude.
 
 
 for n=1:Tfinal/T
     
-    %stimulus
+    % input stimulus
     if n < Tfinal/(4*T) % apply step after 1/4th of the simulation time
         F = 0;
         %F = sin(2*pi*n*T);
@@ -57,7 +57,7 @@ for n=1:Tfinal/T
         F = 0;
     end
 
-    % discrete adaptive Hopf
+    % discretized adaptive Hopf
     x_d = gamma*( mu-(x^2 + y^2) )*x - ohm*y + Epsilon*F;
     y_d = gamma*( mu-(x^2 + y^2) )*y + ohm*x;
     ohm_d = -Epsilon * F * y/( sqrt(x^2 + y^2) );
@@ -65,9 +65,9 @@ for n=1:Tfinal/T
     y_new = y + T*y_d;
     ohm_new = ohm + T*ohm_d;
     
-    plotvar1(n) = F; %input stimulus
-    plotvar2(n) = x; %output
-    plotvar3(n) = ohm/(2*pi); %frequency
+    plotvar1(n) = F; % input stimulus
+    plotvar2(n) = x; % output
+    plotvar3(n) = ohm/(2*pi); % frequency
     
     x = x_new;
     y = y_new;
